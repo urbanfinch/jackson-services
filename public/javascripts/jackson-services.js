58560d4788fae08fc2b318f470df02fd5c37472d
Aaron Wright <acwrightdesign@gmail.com>
2012-03-05 22:21:51 -0600

    Fix for IE.

--- public/javascripts/jackson-services.js
+++ public/javascripts/jackson-services.js
@@ -24,16 +24,18 @@ var jackson_services = {
       return;
     }
     
-    $('input[placeholder]').each(function (){
+    $.merge($('input[placeholder]'), $('textarea[placeholder]')).each(function (){
       var placeholderName = $(this).attr('id');
       var placeholderText = $(this).attr('placeholder');
       var placeholder = $('<label for='+ $(this).attr('id') +'>'+ placeholderText + '</label>');
-      placeholder.attr('style', 'position: absolute; left: 8px; top: 8px; font-size: 10px; color: #AAAAAA;');
+      placeholder.attr('style', 'position: absolute; cursor: text; left: 8px; top: 8px; font-size: 10px; color: #AAAAAA;');
       
       $(this).before(placeholder);
       
       $(placeholder).click(function(){
         $('label[for=' + placeholderName + ']').hide();
+        document.getElementById(placeholderName).focus();
+        //$('label[for=' + placeholderName + ']').triggerHandler('focus');
       })
       $(this).focus(function(){
         $('label[for=' + placeholderName + ']').hide();
